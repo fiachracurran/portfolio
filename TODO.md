0544158a5f556a16a059b5e9e581431fe4f44181
Fiachra Curran <fiachracurran@gmail.com>
2024-12-14 23:30:13 +0000

    implement all feedback from 04/12/2024

--- TODO.md
+++ TODO.md
@@ -1,17 +1,12 @@
 # Interactive Media Design CA2
 
 ### TODO
-- [ ] Add social media icons and links on About Me page  
-- [ ] Include e-mail on Contact Me page  
-- [ ] Add Google Maps integration in footer  
-- [ ] Add Contact Me form popup  
 - [ ] Host site  
-  - [ ] Update fontawesome kit with domain of hosted site  
-- [ ] Finish Project section  
-- [ ] Add animations to make site more responsive/interactive  
+  - [ ] Update fontawesome kit with domain of hosted site   
+  - [ ] Restrict Google API key with domain of hosted site
 
 ### Ongoing
-- [ ] Update README.md with description of how site meets CA2's objectives  
+- [~] Update README.md with description of how site meets CA2's objectives  
 
 ### Completed ✓
 - [X] Create skeleton for portfolio site  
@@ -20,3 +15,19 @@
 - [X] Add CV download  
 - [X] Add picture on About Me page  
 - [X] Add picture on Home Page 
+- [X] Make each page vh-100 / min-vh-100
+- [X] Add Email Me btn to home page
+- [X] Fill out Experience page
+- [X] Add carousel to Achievements page
+- [X] Add Contact Me form popup  
+- [X] Add animations to make site more responsive/interactive  
+- [X] Fetch from NASA APOD API
+- [X] Add Google Maps integration in footer
+- [X] Finish Project section 
+- [X] Add social media icons and links on About Me page  
+- [X] Display correctly on mobiles and tablets
+- [X] Implement ALL feedback given on 04/12/2024
+
+### Discarded
+- [ ] Include e-mail on Contact Me page  
+- [ ] Sitemap (doesn't seem necessary/needed for a single-page site)  
--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,44 @@
-let scrollSpy = new bootstrap.scrollSpy(document.body, { target: "navbar-main" });
+window.onload = function() {
+    const scrollSpy = new bootstrap.ScrollSpy(document.body, {
+        target: '#navbar-main'
+    });
+    
+    fetchNasa();
+    handleContactForm();
+}
+
+async function fetchNasa() {  // 
+    const url = "https://api.nasa.gov/planetary/apod?api_key=VWUtd74fXVnfLYDGfmI4VQv2gDBH2z2ORnUMdqTs";
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+  
+      const data = await response.json();
+      console.log(data);
+      const target = document.getElementById('nasa-apod');
+      target.src = data.url;
+    } catch (error) {
+      console.error(error.message);
+    }
+}
+
+function handleContactForm() {  //
+    const contactForm = document.getElementById('contact-form');
+    const thankYouMessage = document.getElementById('thank-you-message');
+    contactForm.addEventListener('submit', (e) => {
+        e.preventDefault();  // prevents form clear (default behaviour) so that Thank You message can be displayed
+
+        thankYouMessage.classList.add('show');
+        contactForm.classList.add('d-none');  // d-none is Bootstrap class for display:none
+        
+        contactForm.reset();  // resets the form
+
+        // restores visibility of contactForm after 3 seconds
+        setTimeout(function() {
+            thankYouMessage.classList.remove('show');
+            contactForm.classList.remove('d-none');
+        }, 3000);
+    });
+}
